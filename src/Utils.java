3ebfe33cf0bcd38f38e18ae5d118adf8dcaae827
Haris Qureshi <m-harisqurashi@users.noreply.github.com>
2018-01-16 18:14:06 +0500

    Update Utils.java
    Added date functions

--- src/Utils.java
+++ src/Utils.java
@@ -1,8 +1,17 @@
 
 public class Utils {
+	
+    public static Locale getLocale() {
+        return Locale.US;
+    }
 
-	public Utils() {
-		
-	}
+    public static Calendar getCalendar() {
+        Calendar calendar = Calendar.getInstance(getLocale());
+        calendar.setFirstDayOfWeek(Calendar.SUNDAY);
+        return calendar;
+    }
 
+    public static String convertDate(long dateInMilliseconds, String dateFormat) {
+        return (new SimpleDateFormat(dateFormat, Locale.getDefault()).format(new Date(dateInMilliseconds)));
+    }
 }
